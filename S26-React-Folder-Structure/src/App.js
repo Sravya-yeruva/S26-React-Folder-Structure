00c2309507d6e35e221a5535792af477103144fc
Sravya-Yeruva <yeruvasravya359@gmail.com>
2024-11-17 20:18:17 +0530

    Add files via upload

---
+++ S26-React-Folder-Structure/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chocolate is a food made from roasted and ground cocoa beans that can be a liquid, solid, or paste, either on its own or as a flavoring in other foods.</h1>
+      <img src="https://www.thespruceeats.com/thmb/FhHcgQni8lgV0griUeDJMTAszxI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chocolate_hero1-d62e5444a8734f8d8fe91f5631d51ca5.jpg"></img>
+  
+      </div>
+  );
+}
+
+export default App;
